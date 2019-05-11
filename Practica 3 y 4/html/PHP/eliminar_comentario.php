7e3386f440f0ce4281b30b6aeea6ffd2a47bac1e
juanfrandm98 <juanfrandm98@gmail.com>
2019-05-11 20:59:26 +0200

    Intento de eliminar_comentario

---
+++ Practica 3 y 4/html/PHP/eliminar_comentario.php
@@ -0,0 +1,16 @@
+<?php
+	require_once '../vendor/autoload.php';
+	require( "funciones.php" );
+	include_once( "plantilla_sesion.php" );
+
+	if( isset( $_SESSION['user'] ) ) {
+		if( $_SESSION['user']['tipo'] == "moderador" || $_SESSION['user']['tipo'] == "superusuario" ) {
+
+			if( filter_var( $_GET['id_comentario'], FILTER_VALIDATE_INT ) ) {
+				eliminarComentario( $_GET['id_comentario'] );
+				header( "location:/" );
+			} else header( "location:/" );
+
+		} else header( "location:/" );
+	} else header( "location:/" );
+?>
